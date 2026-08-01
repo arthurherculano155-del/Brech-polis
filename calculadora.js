e6e52d1844e1479ae47d2326a28763f15a0724d7
Arthur Herculano <arthurherculano155@gmail.com>
2026-08-01 17:57:16 -0300

    Remodelei outra página

---
+++ calculadora.js
@@ -0,0 +1,29 @@
+function calcular(){
+    let parcelas = Number(document.getElementById("parcelas").value);
+    let valor = Number(document.getElementById("valor").value);
+    
+    if(parcelas <= 0){
+        document.getElementById("valorParcela").innerHTML = "Impossível dividir por 0.";
+        return;
+    }
+
+    let valorParcela = valor / parcelas;
+
+    let juros = 0;
+    document.getElementById("juros").style.color = "lime";
+
+    if(parcelas >= 10){
+        juros = 0.15;
+
+        document.getElementById("juros").style.color = "red";
+
+        valor += (valor * juros);
+        valorParcela += (valorParcela * juros);
+    }
+
+
+    document.getElementById("juros").innerHTML = `${juros*100}%`;
+    document.getElementById("valorParcela").innerText = `R$ ${valorParcela.toFixed(2)}`;
+    document.getElementById("valorTotal").innerText = `R$ ${valor.toFixed(2)}`;
+
+}
