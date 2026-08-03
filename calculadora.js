b28cd9df43d4ee0f6104470ce1b03594bc99ff28
Arthur Herculano <arthurherculano155@gmail.com>
2026-08-03 05:23:58 -0300

    Adicionei mais funcionalidades na calculadora

--- calculadora.js
+++ calculadora.js
@@ -2,35 +2,42 @@ function calcular(){
     let parcelas = Number(document.getElementById("parcelas").value);
     let valor = Number(document.getElementById("valor").value);
     
-    if(parcelas <= 0 || parcelas >= 20){
+    if(parcelas <= 0 || parcelas >= 20){    
+        document.getElementById("valorParcela").style.fontSize = "1.3rem";
+
+
         document.getElementById("valorParcela").innerHTML = "Insira um número válido de parcelas.";
+        
+
+        document.getElementById("valorTotal").innerText = "R$ 0.00";
+
         return;
-    } 
-
-    let valorParcela = valor / parcelas;
-
-    let juros = 0;
-    document.getElementById("juros").style.color = "lime";
-
-    if(parcelas >= 15){
-        juros = 0.25;
-    } else if(parcelas >= 10){
-        juros = 0.15;
-    } else if(parcelas >= 5){
-        juros = 0.05;
     }
 
+        let valorParcela = valor / parcelas;
 
-    if(juros > 0){
-        document.getElementById("juros").style.color = "red";
+        let juros = 0;
+        document.getElementById("juros").style.color = "lime";
 
-        valor += (valor * juros);
-        valorParcela += (valorParcela * juros);
-    }
+        if(parcelas >= 15){
+            juros = 0.25;
+        } else if(parcelas >= 10){
+            juros = 0.15;
+        } else if(parcelas >= 5){
+            juros = 0.05;
+        }
 
 
-    document.getElementById("juros").innerHTML = `${juros*100}%`;
-    document.getElementById("valorParcela").innerText = `R$ ${valorParcela.toFixed(2)}`;
-    document.getElementById("valorTotal").innerText = `R$ ${valor.toFixed(2)}`;
+        if(juros > 0){
+            document.getElementById("juros").style.color = "red";
+
+            valor += (valor * juros);
+            valorParcela += (valorParcela * juros);
+        }
+
+
+        document.getElementById("juros").innerHTML = `${juros*100}%`;
+        document.getElementById("valorParcela").innerText = `R$ ${valorParcela.toFixed(2)}`;
+        document.getElementById("valorTotal").innerText = `R$ ${valor.toFixed(2)}`;
 
 }
