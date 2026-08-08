23814f56b3f6b0b923eb06446b629a1ef197d2c2
Arthur Herculano <arthurherculano155@gmail.com>
2026-08-08 12:34:07 -0300

    Resolvi erros de push

--- calculadora.js
+++ calculadora.js
@@ -1,59 +1,65 @@
-function calcular(){
+function calcular() {
+
+    document.getElementById("valorParcela").style.color = "lime";
 
     document.getElementById("valorParcela").style.fontSize = "2rem";
 
     let parcelas = Number(document.getElementById("parcelas").value);
 
     let valor = Number(document.getElementById("valor").value);
-    
-    if(parcelas <= 0 || parcelas >= 20){    
+
+    if (parcelas <= 0 || parcelas > 20) {
+
         document.getElementById("valorParcela").style.fontSize = "1.3rem";
 
 
         document.getElementById("valorParcela").innerHTML = "Insira um número válido de parcelas.";
-        
+        document.getElementById("valorParcela").style.color = "red";
 
-        document.getElementById("valorTotal").innerText = "R$ 0.00";
+
+        document.getElementById("valorTotal").innerText = "R$ 0,00";
 
         return;
     }
 
 
 
-        let valorParcela = valor / parcelas;
+    let valorParcela = valor / parcelas;
 
-        let juros = 0;
-        document.getElementById("juros").style.color = "lime";
+    let juros = 0;
+    document.getElementById("juros").style.color = "lime";
 
-        if(parcelas >= 15){
-            juros = 0.25;
-        } else if(parcelas >= 10){
-            juros = 0.15;
-        } else if(parcelas >= 5){
-            juros = 0.05;
-        }
-
-
-        if(juros > 0){
-            document.getElementById("juros").style.color = "red";
-
-            valor += (valor * juros);
-            valorParcela += (valorParcela * juros);
-        }
-
-        let valorFinalParcela = Math.trunc(valorParcela * 100) / 100;
-
-        let valorFinalTotal= Math.trunc(valor * 100) / 100;
+    if (parcelas >= 20) {
+        juros = 0.5;
+    } else if (parcelas >= 15) {
+        juros = 0.25;
+    } else if (parcelas >= 10) {
+        juros = 0.15;
+    } else if (parcelas >= 5) {
+        juros = 0.05;
+    }
 
 
-        document.getElementById("juros").innerHTML = `${juros*100}%`;
-        document.getElementById("valorParcela").innerText = `R$ ${valorFinalParcela.toLocaleString("pt-BR", {
-            minimumFractionDigits: 2,
-            maximumFractionDigits: 2
-        })}`;
-        document.getElementById("valorTotal").innerText = `R$ ${valorFinalTotal.toLocaleString("pt-BR", {
-            minimumFractionDigits: 2,
-            maximumFractionDigits: 2
-        })}`;
+    if (juros > 0) {
+        document.getElementById("juros").style.color = "red";
+
+        valor += (valor * juros);
+        valorParcela += (valorParcela * juros);
+    }
+
+    let valorFinalParcela = Math.trunc(valorParcela * 100) / 100;
+
+    let valorFinalTotal = Math.trunc(valor * 100) / 100;
+
+
+    document.getElementById("juros").innerHTML = `${juros * 100}%`;
+    document.getElementById("valorParcela").innerText = `R$ ${valorFinalParcela.toLocaleString("pt-BR", {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+    })}`;
+    document.getElementById("valorTotal").innerText = `R$ ${valorFinalTotal.toLocaleString("pt-BR", {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+    })}`;
 
 }
