5d0422eabba82c482d9512767974a332bc85681f
Arthur <arthurherculano155@gmail.com>
2026-09-21 23:22:29 -0300

    Organizei menu

--- brechopolis/src/Components/Shared/Menu/menu.jsx
+++ brechopolis/src/Components/Shared/Menu/menu.jsx
@@ -4,6 +4,7 @@ import { NavLink } from 'react-router-dom';
 export default function Menu() {
     return (
         <header className="menu">
+
             <img
                 src="/Assets/Images/brechopolis-logo.png"
                 alt="Logo Brechópolis"
@@ -11,6 +12,7 @@ export default function Menu() {
             />
 
             <ul className="links">
+
                 <li>
                     <a
                         href="https://www.google.com/maps/place/B.+Luxo/@-23.5317407,-46.6622659,17z/data=!3m1!4b1!4m6!3m5!1s0x94ce59d3efc9f233:0xcd7460a15000a1ec!8m2!3d-23.5317407!4d-46.6622659!16s%2Fg%2F1ptwxd6mg"
@@ -22,42 +24,56 @@ export default function Menu() {
                 </li>
 
                 <li>
-                    <NavLink 
+                    <NavLink
                         to="/"
                         end
-                        className={({isActive}) => 
+                        className={({ isActive }) =>
                             isActive ? "Ativo" : ""
-                        }>Início</NavLink>
+                        }
+                    >
+                        Início
+                    </NavLink>
                 </li>
 
                 <li>
-                    <NavLink    
+                    <NavLink
                         to="/conjuntos"
-                        className={({isActive}) => 
-                        isActive ? "Ativo" : ""
-                        }>Conjuntos</NavLink>
+                        className={({ isActive }) =>
+                            isActive ? "Ativo" : ""
+                        }
+                    >
+                        Conjuntos
+                    </NavLink>
                 </li>
 
                 <li>
-                    <NavLink    
+                    <NavLink
                         to="/retirada"
-                        className={({isActive}) => 
-                        isActive ? "Ativo" : ""
-                        }>Retirada</NavLink>
+                        className={({ isActive }) =>
+                            isActive ? "Ativo" : ""
+                        }
+                    >
+                        Retirada
+                    </NavLink>
                 </li>
 
                 <li>
-                    <NavLink    
+                    <NavLink
                         to="/contato"
-                        className={({isActive}) => 
-                        isActive ? "Ativo" : ""
-                        }>Contato</NavLink>
+                        className={({ isActive }) =>
+                            isActive ? "Ativo" : ""
+                        }
+                    >
+                        Contato
+                    </NavLink>
                 </li>
+
             </ul>
 
             <button className="login-button">
                 <i className="fa-regular fa-user"></i>
             </button>
+
         </header>
-    )
+    );
 }
